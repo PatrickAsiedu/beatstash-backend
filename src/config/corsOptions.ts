a4159d846c55226977630abae01a6d08b68223f9
PatrickAsiedu <pattribette@gmail.com>
2025-11-30 19:47:47 +0000

    debugging cors issue

--- src/config/corsOptions.ts
+++ src/config/corsOptions.ts
@@ -5,9 +5,9 @@ type origin = string | undefined;
 const CorsOptions = {
   origin: (origin: origin, callback: any) => {
     // Allow requests with no origin (like mobile apps or Postman)
-    if (!origin) {
-      return callback(null, true);
-    }
+    // if (!origin) {
+    //   return callback(null, true);
+    // }
 
     if (allowedOrigins.indexOf(origin) !== -1) {
       callback(null, true);
